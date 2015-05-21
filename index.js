0ad1cf12cc8a9a4949d58f573fb219c23405217e
William Buchwalter <wbuchwalter@gmail.com>
2015-05-21 00:07:44 -0400

    Simplification

--- index.js
+++ index.js
@@ -4,57 +4,35 @@
 	based on jshint-loader by Tobias Koppers
 */
 var Linter = require("tslint");
-var RcLoader = require("rcloader");
 var stripJsonComments = require("strip-json-comments");
 var loaderUtils = require("loader-utils");
 var fs = require("fs");
+var path = require("path");
+var typescript = require("typescript");
 
-
-var rcLoader = new RcLoader("tslint.json", null, {
-	loader: function(path) {
-		return path;
-	}
-});
-
-function loadRelativeConfig(callback){
-	var sync = typeof callback !== "function";
-
-	if(sync){
-		var path = rcLoader.for(this.resourcePath);
-		if(typeof path !== "string") {
-			// no tslint.json found
-			return {};
-		} else {
-			this.addDependency(path);
-			var file = fs.readFileSync(path, "utf8");
-			return JSON.parse(stripJsonComments(file));
-		}
-	}
-	else {
-		rcLoader.for(this.resourcePath, function(err, path) {
-			if(typeof path !== "string") {
-				// no tslint.json found
-				return callback(null, {});
-			}
-
-			this.addDependency(path);
-			fs.readFile(path, "utf8", function(err, file) {
-				var options;
-
-				if(!err) {
-					try {
-						options = JSON.parse(stripJsonComments(file));
-					}
-					catch(e) {
-						err = e;
-					}
-				}
-				callback(err, options);
-			});
-		}.bind(this));
+function loadRelativeConfig(){
+	var configPath = locateConfigFile("tsconfig.json", path.dirname(this.resourcePath));
+	if(typeof configPath !== "string") {
+		// no tslint.json found
+		console.log('tslint.json not found');
+		return {};
+	} else {
+		this.addDependency(configPath);
+		var file = fs.readFileSync(path, "utf8");
+		return JSON.parse(stripJsonComments(file));
 	}
 }
 
+function locateConfigFile(filename, startingPath){
+	var filePath = path.join(startingPath, filename);
+	if(typescript.sys.fileExists(filePath)){
+		return filePath;
+	}
+	var parentPath = path.dirname(startingPath);
+	if(parentPath === startingPath)
+		return undefined;
+	locateConfigFile(filename,parentPath);
+}
 
 function lint(input, config) {
 	
@@ -72,8 +50,7 @@ function lint(input, config) {
 	}
 	
 	var source = input.split(/\r\n?|\n/g);
-	var linter = new Linter(this.path, source, config);
-	
+	var linter = new Linter(this.resourcePath, source, config);
 	var result = linter.lint();	
 	this.emitWarning('tslint: ' + result);
 }
@@ -83,24 +60,9 @@ module.exports = function(input, map) {
 	this.cacheable && this.cacheable();
 	var callback = this.async();
 
-	if(!callback) {
-		// load tslint.json synchronously
-		var config = loadRelativeConfig.call(this);
-		lint.call(this, input, config);
-		return input;
-	}
-
-	// load tslint.json asynchronously
-	loadRelativeConfig.call(this, function(err, config) {
-		if(err) return callback(err);
-
-		try {
-			lint.call(this, input, config);
-		}
-		catch(e) {
-			return callback(e);
-		}
-		callback(null, input, map);
-
-	}.bind(this));
+	// load tslint.json synchronously
+	var config = loadRelativeConfig.call(this);
+	lint.call(this, input, config);	
+	callback(null, input, map);
 }
+
